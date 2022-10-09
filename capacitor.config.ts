0ed44ebcb11265a5a1d85a971ef1b6b65afab763
Juan Thielmann <meganitrospeed@gmail.com>
2022-10-09 17:49:10 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ip_history_web',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
